b56fd082ccc55d080c01df0318fe74c23c97e4bd
kobatakeyann <suu_hppa@outlook.jp>
2024-08-11 18:14:31 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
+from datetime import date
+
+from constants.constant import is_surface
+from figure.drawing.plot import FigureFactory
+
 if __name__ == "main":
-    pass
+    target_date = date(2023, 8, 21)
+    factory = FigureFactory(utc_date=target_date, is_surface=is_surface)
+    factory.make_whole_day_figures()
