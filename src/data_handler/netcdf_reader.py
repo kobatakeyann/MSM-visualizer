512608bb4c30d646e8d9e0ca1e81f2985426250a
kobatakeyann <suu_hppa@outlook.jp>
2024-08-11 18:04:57 +0900

    Update netcdf reader method

--- src/data_handler/netcdf_reader.py
+++ src/data_handler/netcdf_reader.py
@@ -1,6 +1,14 @@
+import numpy as np
 import xarray as xr
 
-from constants.constant import LAT_BOTTOM, LAT_TOP, LON_LEFT, LON_LIGHT
+from constants.constant import (
+    LAT_BOTTOM,
+    LAT_TOP,
+    LON_LEFT,
+    LON_RIGHT,
+    PRESSURE_PLAIN,
+    is_surface,
+)
 
 
 class NetcdfHandler:
@@ -12,12 +20,18 @@ class NetcdfHandler:
         self,
         ds: xr.Dataset,
     ) -> xr.Dataset:
+        if not is_surface:
+            ds = ds.sel(p=PRESSURE_PLAIN)
         ds = ds.isel(time=0)
         ds = ds.sel(
-            lon=slice(LON_LEFT, LON_LIGHT), lat=slice(LAT_TOP, LAT_BOTTOM)
+            lon=slice(LON_LEFT, LON_RIGHT), lat=slice(LAT_TOP, LAT_BOTTOM)
         )
         return ds
 
     def get_lon_lat_array(self) -> None:
         self.lon = self.ds["lon"].values
         self.lat = self.ds["lat"].values
+
+    def extract_data_array(self, var_name: str) -> np.ndarray:
+        target_array = self.ds[var_name].values
+        return target_array
