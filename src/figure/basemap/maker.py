8958aabddb2e13cd5ae83c66aef4338e71960e64
kobatakeyann <suu_hppa@outlook.jp>
2024-08-11 22:16:37 +0900

    Update import sentence

--- src/figure/basemap/maker.py
+++ src/figure/basemap/maker.py
@@ -5,7 +5,7 @@ from constants.constant import is_deg_min_format
 from figure.basemap.methods import Basemap
 
 
-def make_base_map_ax(ax: GeoAxes) -> GeoAxes:
+def make_base_map(ax: GeoAxes) -> GeoAxes:
     mplstyle.use("fast")
     basemap_ax = Basemap(ax)
     basemap_ax.plot_coastline()
