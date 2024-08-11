cdf7d30b80a9520fdef38ce7a6f70c8aaab19a28
kobatakeyann <suu_hppa@outlook.jp>
2024-08-11 22:19:19 +0900

    Update title text

--- src/figure/helper/text.py
+++ src/figure/helper/text.py
@@ -1,12 +1,14 @@
-from constants.constant import TITLE
+from constants.configuration import PRESSURE_PLAIN, TITLE, is_surface
 from helper.time import PaddingDatetime
 
 
 class TextGenerator(PaddingDatetime):
     def get_title_text(self) -> str:
-        title = (
-            f"{self.year}/{self.month}/{self.day} {self.hour}00UTC  {TITLE}"
-        )
+        if is_surface:
+            plain = "surface"
+        else:
+            plain = f"{PRESSURE_PLAIN}hPa"
+        title = f"{self.year}/{self.month}/{self.day} {self.hour}00UTC  {plain} {TITLE}"
         return title
 
     def get_filename(self) -> str:
