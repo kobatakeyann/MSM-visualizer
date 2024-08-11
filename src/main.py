eecb8e302f4ad6113c7ccb38e8966d6b4a3a789e
kobatakeyann <suu_hppa@outlook.jp>
2024-08-11 22:20:07 +0900

    Change execution date

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from datetime import date
 
-from constants.constant import is_surface
+from constants.configuration import is_surface
 from figure.drawing.plot import FigureFactory
 
 if __name__ == "__main__":
-    target_date = date(2023, 8, 21)
+    target_date = date(2015, 8, 28)
     factory = FigureFactory(utc_date=target_date, is_surface=is_surface)
     factory.make_whole_day_figures()
