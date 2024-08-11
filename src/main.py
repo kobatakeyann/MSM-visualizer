0e01a94de63647d127138bcbe87b6bc3f576bc7a
kobatakeyann <suu_hppa@outlook.jp>
2024-08-11 18:18:23 +0900

    Fix if __name section

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from datetime import date
 from constants.constant import is_surface
 from figure.drawing.plot import FigureFactory
 
-if __name__ == "main":
+if __name__ == "__main__":
     target_date = date(2023, 8, 21)
     factory = FigureFactory(utc_date=target_date, is_surface=is_surface)
     factory.make_whole_day_figures()
